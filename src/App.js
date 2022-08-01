d678980b735ac2586fc48b011795eba2653d2faf
Henri Forss <hforss@gmail.com>
2022-08-01 13:21:33 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import "./App.css"
 
+
+/* API key. */
+const API_KEY = process.env.REACT_APP_API_KEY
+
+/* The App itself. */
 function App() {
+
+  /* Define variables with useState(). */
+  const [stockData, setStockData] = useState(null)
+
+  /* Get the data. */
+  const getData = () => {
+    fetch(`https://data.nasdaq.com/api/v3/datatables/QUOTEMEDIA/PRICES?ticker=AAPL&api_key=${API_KEY}`)
+      .then((response) => {
+        return response.json()   
+      })
+      .then((data) => {
+        setStockData(data)
+        console.log(data)
+      })    
+  }
+    
+  /* Call getData on load. */
+  useEffect(getData, [])
+    
+  /* Return stuff. */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Morjens!
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
